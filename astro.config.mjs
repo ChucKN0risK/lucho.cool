b9a7a44f37ca124e8bd6cc3609e9f372a20ebc33
Louis Chenais <lchenais@gmail.com>
2024-10-01 20:24:59 +0200

    Remove rehype-autolink-headings

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,29 +7,11 @@ import rehypeAutolinkHeadings from 'rehype-autolink-headings';
 export default defineConfig({
   markdown: {
     rehypePlugins: [
-
       [
         // The ' ' value is the only way to
         // get rid of the class :/
         rehypeFigure, { className: ' ' }
-      ],
-      rehypeSlug,
-      [
-        rehypeAutolinkHeadings,
-        {
-          behavior: 'prepend',
-          content: {
-            type: 'text',
-            value: '#',
-          },
-          headingProperties: {
-            className: ['anchor'],
-          },
-          properties: {
-            className: ['anchor-link'],
-          },
-        },
-      ],
+      ]
     ],
   },
 });
