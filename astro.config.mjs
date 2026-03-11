e40bf6cdd3203a67b6b0c0b610f883215cd7220a
Louis Chenais <lchenais@gmail.com>
2026-03-11 08:25:33 +0100

    chore: fix linting issues

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,37 +1,36 @@
-import { defineConfig } from "astro/config";
-import rehypeFigure from "@microflash/rehype-figure";
-import rehypeExternalLinks from "rehype-external-links";
 import sitemap from "@astrojs/sitemap";
-import robotsTxt from "astro-robots-txt";
+import rehypeFigure from "@microflash/rehype-figure";
+import { defineConfig } from "astro/config";
 import icon from "astro-icon";
-
 import og from "astro-og";
+import robotsTxt from "astro-robots-txt";
+import rehypeExternalLinks from "rehype-external-links";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://lucho.cool",
-  markdown: {
-    rehypePlugins: [
-      [
-        rehypeExternalLinks,
-        {
-          rel: ["nofollow, noreferrer"],
-          target: "_blank",
-        },
-      ],
-      [
-        // The ' ' value is the only way to
-        // get rid of the class :/
-        rehypeFigure,
-        { className: " " },
-      ],
-    ],
-  },
-  integrations: [
-    sitemap(),
-    robotsTxt(),
-    icon({ iconDir: "src/assets/vectors" }),
-    og(),
-  ],
-  prefetch: true,
+	site: "https://lucho.cool",
+	markdown: {
+		rehypePlugins: [
+			[
+				rehypeExternalLinks,
+				{
+					rel: ["nofollow, noreferrer"],
+					target: "_blank",
+				},
+			],
+			[
+				// The ' ' value is the only way to
+				// get rid of the class :/
+				rehypeFigure,
+				{ className: " " },
+			],
+		],
+	},
+	integrations: [
+		sitemap(),
+		robotsTxt(),
+		icon({ iconDir: "src/assets/vectors" }),
+		og(),
+	],
+	prefetch: true,
 });
